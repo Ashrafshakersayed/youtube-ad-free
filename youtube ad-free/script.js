7388fc914ada7cb0481084f2063cf076d028c98c
Ashrafshakersayed <73795682+Ashrafshakersayed@users.noreply.github.com>
2021-05-25 11:07:56 +0200

    Add files via upload

---
+++ youtube ad-free/script.js
@@ -0,0 +1,15 @@
+
+    setInterval(function() 
+    { 
+        var cross = document.getElementsByClassName("ytp-ad-overlay-close-container")[0];
+        var skip = document.getElementsByClassName("ytp-ad-skip-button")[0];
+        if (cross != undefined)
+        { 
+            cross.click();
+        }
+        if (skip != undefined) 
+        {
+            skip.click() 
+        }
+    },
+    2000);
